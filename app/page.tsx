c07a39b2832ee2eb9ba3236cc476950d83fb6c2b
ABU DAVID <65656238+davidabuu@users.noreply.github.com>
2023-04-04 23:28:01 +0100

    NavBar responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
+"use client";
 import Navbar from "@/src/compoments/Navbar";
 import UserWebLayout from "@/src/compoments/UserWebLayout";
-
-
 export default function Home() {
   return (
-    <UserWebLayout webtitle='Home Page'>
+    <UserWebLayout webtitle="Home Page">
       <div className="font-sans img-bg bg-cover bg-center  h-screen">
         <Navbar />
       </div>
--- src/compoments/Navbar.tsx
+++ src/compoments/Navbar.tsx
@@ -1,7 +1,10 @@
-
-import { Menu } from "heroicons-react";
-import React from "react";
+import { Menu, X } from "heroicons-react";
+import React, { useState } from "react";
 const Navbar = () => {
+  const [nav, setNav] = useState<boolean>(false);
+  const showNav = () => {
+    setNav(!nav);
+  };
   return (
     <div className="flex justify-around items-center w-full py-6">
       <h1 className="font-bold text-yellow-500 text-3xl">Kally Media Group</h1>
@@ -13,9 +16,25 @@ const Navbar = () => {
         <li>Blog</li>
         <li>Contract</li>
       </ul>
-      <div className="md:hidden">
-        <Menu className="text-white cursor-pointer" />
+      <div className="md:hidden" onClick={showNav}>
+        {!nav ? (
+          <Menu className="text-white cursor-pointer" />
+        ) : (
+          <X className="text-white cursor-pointer" />
+        )}
       </div>
+      <ul
+        className={
+          !nav ? "hidden" : " md:hidden absolute w-full mt-72 bg-[#2e2e37] bg-opacity-70"
+        }
+      >
+        <li className="p-2 border-b-[1px] w-full">Home</li>
+        <li className="p-2 border-b-[1px] w-full">About</li>
+        <li className="p-2 border-b-[1px] w-full">Service</li>
+        <li className="p-2 border-b-[1px] w-full">Portfolio</li>
+        <li className="p-2 border-b-[1px] w-full">Blog</li>
+        <li className="p-2 border-b-[1px] w-full">Contract</li>
+      </ul>
     </div>
   );
 };
